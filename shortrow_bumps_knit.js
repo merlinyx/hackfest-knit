1d70a4c6e9bf5ff55590e92384ab26dd0e608cff
Yuxuan Mei <yuxuan.mei@icloud.com>
2022-07-08 21:25:13 -0700

    add rib

--- shortrow_bumps_knit.js
+++ shortrow_bumps_knit.js
@@ -11,7 +11,7 @@ k.addHeader('Position','Center');
 // swatch variables
 var waste_height = 4;
 var width = 72;
-let Carrier = 8;
+let Carrier = 4;
 k.fabricPresser("auto");
 // bring in carrier using yarn inserting hook
 k.inhook(Carrier); 
@@ -46,19 +46,17 @@ k.releasehook(Carrier);
 /////////////////////////////////////////////////////////////
 // the main piece
 // row 1
-for (var h=0; h<waste_height; h++) {
-    for (var s=width; s>=57; s--) {
-        k.knit("-", "f"+s, Carrier);
-    }
-    for (var s=57; s<=width; s++) {
-        k.knit("+", "f"+s, Carrier);
-    }
-}
 for (var s=width; s>=57; s--) {
     k.knit("-", "f"+s, Carrier);
 }
+// for (var s=57; s<=width; s++) {
+//     k.knit("+", "f"+s, Carrier);
+// }
+// for (var s=width; s>=57; s--) {
+//     k.knit("-", "f"+s, Carrier);
+// }
 // shortrow rows G
-for (var h=0; h<10; h++) {
+for (var h=0; h<5; h++) {
     for (var s=57; s<=63; s++) {
         k.knit("+", "f"+s, Carrier);
     }
@@ -67,16 +65,17 @@ for (var h=0; h<10; h++) {
     }
 }
 // row 2
-for (var h=0; h<waste_height; h++) {
-for (var s=41; s<=63; s++) {
+for (var s=57; s<=63; s++) {
     k.knit("+", "f"+s, Carrier);
 }
+// for (var s=41; s<=63; s++) {
+//     k.knit("+", "f"+s, Carrier);
+// }
 for (var s=63; s>=41; s--) {
     k.knit("-", "f"+s, Carrier);
 }
-}
 // shortrow rows E
-for (var h=0; h<10; h++) {
+for (var h=0; h<5; h++) {
     for (var s=41; s<=47; s++) {
         k.knit("+", "f"+s, Carrier);
     }
@@ -84,17 +83,19 @@ for (var h=0; h<10; h++) {
         k.knit("-", "f"+s, Carrier);
     }
 }
-// row 3
-for (var h=0; h<waste_height; h++) {
-for (var s=25; s<=47; s++) {
+for (var s=41; s<=47; s++) {
     k.knit("+", "f"+s, Carrier);
 }
+// row 3
 for (var s=47; s>=25; s--) {
     k.knit("-", "f"+s, Carrier);
 }
-}
+// for (var s=25; s<=47; s++) {
+//     k.knit("+", "f"+s, Carrier);
+// }
+
 // shortrow rows C
-for (var h=0; h<10; h++) {
+for (var h=0; h<5; h++) {
     for (var s=25; s<=31; s++) {
         k.knit("+", "f"+s, Carrier);
     }
@@ -103,16 +104,17 @@ for (var h=0; h<10; h++) {
     }
 }
 // row 4
-for (var h=0; h<waste_height; h++) {
-for (var s=9; s<=31; s++) {
+for (var s=25; s<=31; s++) {
     k.knit("+", "f"+s, Carrier);
 }
+// for (var s=9; s<=31; s++) {
+//     k.knit("+", "f"+s, Carrier);
+// }
 for (var s=31; s>=9; s--) {
     k.knit("-", "f"+s, Carrier);
 }
-}
 // shortrow rows A
-for (var h=0; h<10; h++) {
+for (var h=0; h<5; h++) {
     for (var s=9; s<=15; s++) {
         k.knit("+", "f"+s, Carrier);
     }
@@ -120,19 +122,100 @@ for (var h=0; h<10; h++) {
         k.knit("-", "f"+s, Carrier);
     }
 }
-for (var s=9; s<=15; s++) {
+// row 5
+for (var s=9; s<=width; s++) {
     k.knit("+", "f"+s, Carrier);
 }
-// row 5
-for (var h=0; h<waste_height; h++) {
-    for (var s=width; s>=9; s--) {
-        k.knit("-", "f"+s, Carrier);
-    }
-    for (var s=9; s<=width; s++) {
-        k.knit("+", "f"+s, Carrier);
-    }
+// for (var s=width; s>=9; s--) {
+//     k.knit("-", "f"+s, Carrier);
+// }
+for (var s=width; s>0; s--) {
+    k.knit("-", "f"+s, Carrier);
+}
+for (var s=1; s<=width; s++) {
+    k.knit("+", "f"+s, Carrier);
 }
 for (var s=width; s>0; s--) {
+    k.knit("-", "f"+s, Carrier);
+}
+// shortrow rows I
+for (var h=0; h<5; h++) {
+for (var s=1; s<=9; s++) {
+    k.knit("+", "f"+s, Carrier);
+}
+for (var s=9; s>=1; s--) {
+    k.knit("-", "f"+s, Carrier);
+}
+}
+// row 6
+for (var s=1; s<=24; s++) {
+    k.knit("+", "f"+s, Carrier);
+}
+for (var s=24; s>=16; s--) {
+    k.knit("-", "f"+s, Carrier);
+}
+// shortrow rows G
+for (var h=0; h<5; h++) {
+    for (var s=16; s<=24; s++) {
+        k.knit("+", "f"+s, Carrier);
+    }
+    for (var s=24; s>=16; s--) {
+        k.knit("-", "f"+s, Carrier);
+    }
+}
+// row 7
+for (var s=16; s<=40; s++) {
+    k.knit("+", "f"+s, Carrier);
+}
+for (var s=40; s>=32; s--) {
+    k.knit("-", "f"+s, Carrier);
+}
+// shortrow rows E
+for (var h=0; h<5; h++) {
+    for (var s=32; s<=40; s++) {
+        k.knit("+", "f"+s, Carrier);
+    }
+    for (var s=40; s>=32; s--) {
+        k.knit("-", "f"+s, Carrier);
+    }
+}
+// row 8
+for (var s=32; s<=56; s++) {
+    k.knit("+", "f"+s, Carrier);
+}
+for (var s=56; s>=48; s--) {
+    k.knit("-", "f"+s, Carrier);
+}
+// shortrow rows C
+for (var h=0; h<5; h++) {
+    for (var s=48; s<=56; s++) {
+        k.knit("+", "f"+s, Carrier);
+    }
+    for (var s=56; s>=48; s--) {
+        k.knit("-", "f"+s, Carrier);
+    }
+}
+// row 9
+for (var s=48; s<=width; s++) {
+    k.knit("+", "f"+s, Carrier);
+}
+for (var s=width; s>=64; s--) {
+    k.knit("-", "f"+s, Carrier);
+}
+// shortrow rows A
+for (var h=0; h<5; h++) {
+    for (var s=64; s<=width; s++) {
+        k.knit("+", "f"+s, Carrier);
+    }
+    for (var s=width; s>=64; s--) {
+        k.knit("-", "f"+s, Carrier);
+    }
+}
+// row 10
+for (var s=64; s<=width; s++) {
+    k.knit("+", "f"+s, Carrier);
+}
+for (var s=width; s>=1; s--) {
     k.knit("-", "f"+s, Carrier);
 }
 /////////////////////////////////////////////////////////////
@@ -149,5 +232,5 @@ for (var h=0; h<waste_height; h++) {
 k.outhook(Carrier);
 
 // write the knitout to a file
-k.write('shortrow_bumps_knit.k');
-console.log('wrote shortrow_bumps_knit.k')
+k.write('shortrow_bumps_knit2.k');
+console.log('wrote shortrow_bumps_knit2.k')
